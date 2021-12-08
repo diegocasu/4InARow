7ffd0f4acb810c97fe27ab916ebdaed43dc56c1b
diegocasu <d.casu3@studenti.unipi.it>
2021-12-08 20:22:16 +0100

    Add END_HANDSHAKE message

---
+++ src/message/EndHandshake.cpp
@@ -0,0 +1,81 @@
+#include <string.h>
+#include <arpa/inet.h>
+#include <ostream>
+#include <Utils.h>
+#include <SerializationException.h>
+#include "EndHandshake.h"
+
+namespace fourinarow {
+
+
+EndHandshake::EndHandshake(std::vector<unsigned char> digitalSignature)
+: digitalSignature(std::move(digitalSignature)) {}
+
+uint8_t EndHandshake::getType() const {
+    return type;
+}
+
+const std::vector<unsigned char>& EndHandshake::getDigitalSignature() const {
+    return digitalSignature;
+}
+
+void EndHandshake::checkIfSerializable() {
+    if (digitalSignature.empty()) {
+        throw SerializationException("Empty digital signature");
+    }
+
+    if (digitalSignature.size() != DIGITAL_SIGNATURE_SIZE) {
+        std::string errorMessage("The digital signature size must be exactly ");
+        errorMessage.append(std::to_string(DIGITAL_SIGNATURE_SIZE));
+        errorMessage.append(" bytes. Digital signature size: ");
+        errorMessage.append(std::to_string(digitalSignature.size()));
+        errorMessage.append(" bytes");
+        throw SerializationException(errorMessage);
+    }
+}
+
+std::vector<unsigned char> EndHandshake::serialize() {
+    checkIfSerializable();
+
+    size_t processedBytes = 0;
+    size_t outputSize = sizeof(type) + digitalSignature.size();
+    std::vector<unsigned char> message(outputSize);
+
+    // Serialize the type.
+    memcpy(message.data(), &type, sizeof(type));
+    processedBytes += sizeof(type);
+
+    // Serialize the digital signature.
+    memcpy(message.data() + processedBytes, digitalSignature.data(), digitalSignature.size());
+
+    return message;
+}
+
+void EndHandshake::deserialize(const std::vector<unsigned char> &message) {
+    size_t processedBytes = 0;
+
+    // Check if the type matches the expected one.
+    uint8_t receivedType;
+    checkIfEnoughSpace(message, processedBytes, sizeof(receivedType));
+    memcpy(&receivedType, message.data(), sizeof(receivedType));
+    processedBytes += sizeof(receivedType);
+
+    if (receivedType != END_HANDSHAKE) {
+        throw SerializationException("Malformed message");
+    }
+
+    // Deserialize the digital signature.
+    checkIfEnoughSpace(message, processedBytes, DIGITAL_SIGNATURE_SIZE);
+    digitalSignature.resize(DIGITAL_SIGNATURE_SIZE);
+    memcpy(digitalSignature.data(), message.data() + processedBytes, DIGITAL_SIGNATURE_SIZE);
+}
+
+}
+
+std::ostream& operator<<(std::ostream &ostream, const fourinarow::EndHandshake &endHandshake) {
+    ostream << "EndHandshake{" << std::endl;
+    ostream << "type=" << fourinarow::convertMessageType(endHandshake.getType()) << ',' << std::endl;
+    ostream << "digitalSignature=" << std::endl << fourinarow::dumpVector(endHandshake.getDigitalSignature());
+    ostream << '}';
+    return ostream;
+}
---
+++ src/message/EndHandshake.h
@@ -0,0 +1,43 @@
+#ifndef INC_4INAROW_ENDHANDSHAKE_H
+#define INC_4INAROW_ENDHANDSHAKE_H
+
+#include <Constants.h>
+#include "Message.h"
+
+namespace fourinarow {
+
+/**
+ * Class representing a END_HANDSHAKE message.
+ */
+class EndHandshake : public Message {
+    private:
+        uint8_t type = END_HANDSHAKE;
+        std::vector<unsigned char> digitalSignature;
+
+        /**
+         * Checks if the message has the expected format for serialization.
+         * @throws SerializationException  if the message has not the expected format.
+         */
+        void checkIfSerializable();
+    public:
+        EndHandshake() = default;
+        explicit EndHandshake(std::vector<unsigned char> digitalSignature);
+        ~EndHandshake() override = default;
+
+        EndHandshake(EndHandshake &&) = default;
+        EndHandshake(const EndHandshake&) = default;
+        EndHandshake& operator=(const EndHandshake&) = default;
+        EndHandshake& operator=(EndHandshake&&) = default;
+
+        uint8_t getType() const;
+        const std::vector<unsigned char>& getDigitalSignature() const;
+
+        std::vector<unsigned char> serialize() override;
+        void deserialize(const std::vector<unsigned char> &message) override;
+};
+
+}
+
+std::ostream& operator<<(std::ostream &ostream, const fourinarow::EndHandshake &endHandshake);
+
+#endif //INC_4INAROW_ENDHANDSHAKE_H
