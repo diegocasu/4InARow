98642c185de316701b2b7ae931d382a010104014
diegocasu <d.casu3@studenti.unipi.it>
2021-12-09 09:46:30 +0100

    Add support for messages composed only of a type

---
+++ src/message/InfoMessage.cpp
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <ostream>
+#include <Utils.h>
+#include <SerializationException.h>
+#include "InfoMessage.h"
+
+namespace fourinarow {
+
+InfoMessage::InfoMessage(uint8_t type) : type(type) {}
+
+uint8_t InfoMessage::getType() const {
+    return type;
+}
+
+std::vector<unsigned char> InfoMessage::serialize() {
+    std::vector<unsigned char> message(sizeof(type));
+    memcpy(message.data(), &type, sizeof(type));
+    return message;
+}
+
+void InfoMessage::deserialize(const std::vector<unsigned char> &message) {
+    if (message.size() != sizeof(type)) {
+        throw SerializationException("Malformed message");
+    }
+    memcpy(&type, message.data(), sizeof(type));
+}
+
+}
+
+std::ostream& operator<<(std::ostream &ostream, const fourinarow::InfoMessage &infoMessage) {
+    ostream << "InfoMessage{";
+    ostream << "type=" << fourinarow::convertMessageType(infoMessage.getType());
+    ostream << '}';
+    return ostream;
+}
---
+++ src/message/InfoMessage.h
@@ -0,0 +1,36 @@
+#ifndef INC_4INAROW_INFOMESSAGE_H
+#define INC_4INAROW_INFOMESSAGE_H
+
+#include <cstdint>
+#include "Message.h"
+
+namespace fourinarow {
+
+/**
+ * Class representing a message composed only of a type
+ * (ex. REQ_PLAYER_LIST, PROTOCOL_VIOLATION, etc.).
+ */
+class InfoMessage : public Message {
+    private:
+        uint8_t type = 0;
+    public:
+        InfoMessage() = default;
+        explicit InfoMessage(uint8_t type);
+        ~InfoMessage() override = default;
+
+        InfoMessage(InfoMessage &&) = default;
+        InfoMessage(const InfoMessage&) = default;
+        InfoMessage& operator=(const InfoMessage&) = default;
+        InfoMessage& operator=(InfoMessage&&) = default;
+
+        uint8_t getType() const;
+
+        std::vector<unsigned char> serialize() override;
+        void deserialize(const std::vector<unsigned char> &message) override;
+};
+
+}
+
+std::ostream& operator<<(std::ostream &ostream, const fourinarow::InfoMessage &infoMessage);
+
+#endif //INC_4INAROW_INFOMESSAGE_H
